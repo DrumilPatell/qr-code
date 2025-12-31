4bac4586801b2479885d25838a0549fcfdb4ba8e
Drumil Patel <drumilpatel0987@gmail.com>
2025-12-31 19:26:13 +0530

    Add sitemap and favicon routes to serve static files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, send_file, Response
+from flask import Flask, render_template, request, send_file, Response, send_from_directory
 import qrcode
 import io
+import os
 
 app = Flask(__name__)
 
@@ -23,6 +24,16 @@ def generate_qr():
     img.save(img_io, 'PNG')
     img_io.seek(0)
     return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='qrcode.png')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    """Serve the static sitemap.xml file"""
+    return send_from_directory('.', 'sitemap.xml', mimetype='application/xml')
+
+@app.route('/favicon.ico')
+def favicon():
+    """Redirect favicon requests to the SVG version"""
+    return send_from_directory('static', 'favicon.svg', mimetype='image/svg+xml')
 
 @app.route('/robots.txt')
 def robots():
